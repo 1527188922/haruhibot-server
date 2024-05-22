0888d6e390f290b0c22d9f3a475c6682786cc962
刘昉 <1527188922@qq.com>
2024-05-22 16:08:11 +0800

    sh脚本设置换行符

--- src/main/java/com/haruhi/botServer/config/WebSocketConfig.java
+++ src/main/java/com/haruhi/botServer/config/WebSocketConfig.java
@@ -1,24 +1,37 @@
 package com.haruhi.botServer.config;
 
+import com.haruhi.botServer.interceptors.HeaderInterceptor;
 import com.haruhi.botServer.interceptors.WebSocketHandshakeInterceptor;
 import com.haruhi.botServer.ws.Server;
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @EnableWebSocket
 @Configuration
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer, WebMvcConfigurer {
+    
     @Value("${server.servlet.ws-path}")
     private String wsPath;
+    @Autowired
+    private HeaderInterceptor headerInterceptors;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new Server(), Strings.isBlank(wsPath) ? "/ws" : wsPath)
                 .addInterceptors(new WebSocketHandshakeInterceptor())
                 .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(headerInterceptors)
+                .addPathPatterns("/**");
     }
 }
---
+++ src/main/java/com/haruhi/botServer/interceptors/HeaderInterceptor.java
@@ -0,0 +1,16 @@
+package com.haruhi.botServer.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class HeaderInterceptor implements HandlerInterceptor {
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+}
