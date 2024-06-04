838978fa9f7913431f5d19900a930e7d015d08be
刘昉 <1527188922@qq.com>
2024-06-04 18:01:29 +0800

    test

--- src/main/java/com/haruhi/botServer/utils/CommonUtil.java
+++ src/main/java/com/haruhi/botServer/utils/CommonUtil.java
@@ -1,17 +1,35 @@
 package com.haruhi.botServer.utils;
 
+import cn.hutool.core.img.gif.AnimatedGifEncoder;
+import cn.hutool.core.img.gif.GifDecoder;
 import com.haruhi.botServer.constant.CqCodeTypeEnum;
 import com.haruhi.botServer.constant.RegexEnum;
 import com.simplerobot.modules.utils.KQCodeUtils;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.util.CollectionUtils;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
@@ -172,5 +190,159 @@ public class CommonUtil {
     public static String getAvatarUrl(Long qq, boolean origin){
         return origin ? MessageFormat.format("https://q1.qlogo.cn/g?b=qq&nk={0}&s=0",String.valueOf(qq))
                 : MessageFormat.format("https://q2.qlogo.cn/headimg_dl?dst_uin={0}&spec=100",String.valueOf(qq));
+    }
+
+    public static void main(String[] args) {
+        // GIF图片文件路径  
+        String gifFilePath = "D:\\my\\bot\\resources\\download.gif";
+        // 叠加的图片文件路径  
+        String overlayImagePath = "D:\\my\\bot\\resources\\g.jpg"; // 或 .png  
+        // 新的GIF图片输出文件路径  
+        String outputGifPath = "D:\\my\\bot\\resources\\download1.gif";
+        
+
+        try {
+
+            BufferedImage bufferedImage = Thumbnails.of(overlayImagePath)
+                    .size(110, 110) // 设置目标图片的宽度和高度为200x200像素  
+                    .asBufferedImage();
+
+            GifDecoder gifDecoder = new GifDecoder();
+            gifDecoder.read(new FileInputStream(gifFilePath));
+            int n = gifDecoder.getFrameCount();
+            List<BufferedImage> frames = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                BufferedImage frame = gifDecoder.getFrame(i);  // 原gif的帧
+                // 在指定位置叠加图片（这里假设是(100, 100)）  
+                Graphics2D g2d = frame.createGraphics();
+//                BufferedImage overlayImage = ImageIO.read(new File(overlayImagePath));
+                BufferedImage circularOverlay = makeImageCircular(bufferedImage);
+
+                g2d.drawImage(frame, 0, 0, null);
+//                g2d.drawImage(overlayImage, 100, 100, null);
+                g2d.drawImage(circularOverlay, 100, 100, null);
+                g2d.dispose();
+
+                frames.add(frame);
+            }
+
+            File output = new File(outputGifPath);
+            AnimatedGifEncoder animatedGifEncoder = new AnimatedGifEncoder();
+            animatedGifEncoder.start(new FileOutputStream(output));
+            animatedGifEncoder.setDelay(gifDecoder.getDelay(0));
+            animatedGifEncoder.setRepeat(0);
+            for (BufferedImage image : frames) {
+                animatedGifEncoder.addFrame(image);
+            }
+            animatedGifEncoder.finish();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private static BufferedImage makeImageCircular(BufferedImage image) {
+        int w = image.getWidth();
+        int h = image.getHeight();
+
+        // 取较短的一边作为圆的直径  
+        int diameter = Math.min(w,h);
+
+        // 创建一个新的正方形BufferedImage，用于存放圆形图片  
+        BufferedImage result = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = result.createGraphics();
+
+        // 绘制圆形  
+        Shape shape = new Ellipse2D.Double(0, 0, diameter, diameter);
+        g2d.setClip(shape);
+
+        // 将原始图片绘制到圆形区域  
+        g2d.drawImage(image, (w - diameter) / 2, (h - diameter) / 2, null);
+        g2d.dispose();
+
+        // 裁剪多余部分  
+        return result.getSubimage(0, 0, diameter, diameter);
+    }
+
+    public static BufferedImage cutImage1(BufferedImage originalImage){
+        BufferedImage canvas = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = canvas.createGraphics();
+        int centerX = originalImage.getWidth() / 2;
+        int centerY = originalImage.getHeight() / 2;
+        int radius = Math.min(centerX, centerY);
+        graphics.setComposite(AlphaComposite.Clear);
+        graphics.fillOval(centerX - radius, centerY - radius, radius * 2, radius * 2);
+        graphics.setComposite(AlphaComposite.Src);
+        graphics.drawImage(originalImage, 0, 0, null);
+        graphics.dispose();
+        return canvas;
+    }
+
+
+    public static BufferedImage cutImages(BufferedImage avatarImage) {
+        try {
+            avatarImage = scaleByPercentage(avatarImage, avatarImage.getWidth(),  avatarImage.getWidth());
+            int width = avatarImage.getWidth();
+            // 透明底的图片
+            BufferedImage formatAvatarImage = new BufferedImage(width, width, BufferedImage.TYPE_4BYTE_ABGR);
+            Graphics2D graphics = formatAvatarImage.createGraphics();
+            //把图片切成一个园
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            //留一个像素的空白区域，这个很重要，画圆的时候把这个覆盖
+            int border = 1;
+            //图片是一个圆型
+            Ellipse2D.Double shape = new Ellipse2D.Double(border, border, width - border * 2, width - border * 2);
+            //需要保留的区域
+            graphics.setClip(shape);
+            graphics.drawImage(avatarImage, border, border, width - border * 2, width - border * 2, null);
+            graphics.dispose();
+            //在圆图外面再画一个圆
+            //新创建一个graphics，这样画的圆不会有锯齿 带白边
+//            graphics = formatAvatarImage.createGraphics();
+//            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            int border1 = 3;
+            //画笔是4.5个像素，BasicStroke的使用可以查看下面的参考文档
+            //使画笔时基本会像外延伸一定像素，具体可以自己使用的时候测试
+//            Stroke s = new BasicStroke(5F, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+//            graphics.setStroke(s);
+//            graphics.setColor(Color.WHITE);
+//            graphics.drawOval(border1, border1, width - border1 * 2, width - border1 * 2);
+//            graphics.dispose();
+            return formatAvatarImage;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 缩小Image，此方法返回源图像按给定宽度、高度限制下缩放后的图像
+     *
+     * @param inputImage
+     *            ：压缩后宽度
+     *            ：压缩后高度
+     * @throws java.io.IOException
+     *             return
+     */
+    public static BufferedImage scaleByPercentage(BufferedImage inputImage, int newWidth, int newHeight){
+        // 获取原始图像透明度类型
+        try {
+            int type = inputImage.getColorModel().getTransparency();
+            int width = inputImage.getWidth();
+            int height = inputImage.getHeight();
+            // 开启抗锯齿
+            RenderingHints renderingHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            // 使用高质量压缩
+            renderingHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            BufferedImage img = new BufferedImage(newWidth, newHeight, type);
+            Graphics2D graphics2d = img.createGraphics();
+            graphics2d.setRenderingHints(renderingHints);
+            graphics2d.drawImage(inputImage, 0, 0, newWidth, newHeight, 0, 0, width, height, null);
+            graphics2d.dispose();
+            return img;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
