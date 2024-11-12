77f0a2638ada8e163e9b28fac4ce0c94192b076a
刘昉 <1527188922@qq.com>
2024-11-12 09:44:40 +0800

    点赞完善

--- src/main/java/com/haruhi/botServer/entity/SendLikeRecord.java
+++ src/main/java/com/haruhi/botServer/entity/SendLikeRecord.java
@@ -1,7 +1,22 @@
 package com.haruhi.botServer.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.haruhi.botServer.config.DataBaseConfig;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
+@TableName(value = DataBaseConfig.T_SEND_LIKE_RECORD)
 public class SendLikeRecord {
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private Long userId;
+    private Long selfId;
+    private Date sendTime;
+    private Integer times;
+    private String messageType;
 }
--- src/main/java/com/haruhi/botServer/handlers/message/SendLikeHandler.java
+++ src/main/java/com/haruhi/botServer/handlers/message/SendLikeHandler.java
@@ -1,14 +1,18 @@
 package com.haruhi.botServer.handlers.message;
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.haruhi.botServer.constant.HandlerWeightEnum;
 import com.haruhi.botServer.dto.gocq.response.Message;
 import com.haruhi.botServer.dto.gocq.response.SyncResponse;
+import com.haruhi.botServer.entity.SendLikeRecord;
 import com.haruhi.botServer.event.message.IAllMessageEvent;
 import com.haruhi.botServer.service.sendLikeRecord.SendLikeRecordService;
+import com.haruhi.botServer.utils.DateTimeUtil;
 import com.haruhi.botServer.utils.MatchResult;
 import com.haruhi.botServer.utils.ThreadPoolUtil;
 import com.haruhi.botServer.utils.WsSyncRequestUtil;
+import com.haruhi.botServer.ws.Server;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,6 +36,8 @@ public class SendLikeHandler implements IAllMessageEvent {
     @Autowired
     private SendLikeRecordService sendLikeRecordService;
 
+    private static final int TIMES = 10;
+
     @Override
     public boolean onMessage(WebSocketSession session, Message message) {
         MatchResult result = matches(message);
@@ -40,13 +46,19 @@ public class SendLikeHandler implements IAllMessageEvent {
         }
         ThreadPoolUtil.getHandleCommandPool().execute(()->{
             try {
+                if(isLiked(message.getUserId(), message.getSelfId())){
+                    Server.sendMessage(session,message.getUserId(),message.getGroupId(),message.getMessageType(),"今日已给你点赞过啦",true);
+                    return;
+                }
 
-
-
-                SyncResponse sendLikeRes = WsSyncRequestUtil.sendLike(session, message.getUserId(), 10, 10 * 1000);
+                SyncResponse sendLikeRes = WsSyncRequestUtil.sendLike(session, message.getUserId(), TIMES, 10 * 1000);
                 log.info("发送点赞响应：{}", JSONObject.toJSONString(sendLikeRes));
+                if(sendLikeRes.isSuccess()){
+                    Server.sendMessage(session,message.getUserId(),message.getGroupId(),message.getMessageType(),"攒了你"+TIMES+"次哦，记得回赞",true);
+                    record(message);
+                }
             }catch (Exception e){
-                log.error("error",e);
+                log.error("点赞异常",e);
             }
         });
         return true;
@@ -61,7 +73,22 @@ public class SendLikeHandler implements IAllMessageEvent {
         return MatchResult.unmatched();
     }
 
-    private boolean isLiked(Long userId, Date date){
-        return false;
+    private void record(Message message){
+        SendLikeRecord record = new SendLikeRecord();
+        record.setUserId(message.getUserId());
+        record.setSelfId(message.getSelfId());
+        record.setSendTime(new Date());
+        record.setTimes(TIMES);
+        record.setMessageType(message.getMessageType());
+        sendLikeRecordService.save(record);
+    }
+
+    private boolean isLiked(Long userId, Long selfId){
+        Date d = new Date();
+        int count = sendLikeRecordService.count(new LambdaQueryWrapper<SendLikeRecord>()
+                .eq(SendLikeRecord::getUserId, userId)
+                .eq(SendLikeRecord::getSelfId, selfId)
+                .between(SendLikeRecord::getSendTime, DateTimeUtil.getStartOfDay(d), DateTimeUtil.getEndOfDay(d)));
+        return count > 0;
     }
 }
