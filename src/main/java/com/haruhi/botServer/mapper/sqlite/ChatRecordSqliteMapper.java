06a607000ac29ff98cc19a66d3ff03ed3c38b694
haruhi suzumiya <1527188922@qq.com>
2025-07-05 09:45:16 +0800

    sqlite mapper

--- src/main/java/com/haruhi/botServer/mapper/sqlite/ChatRecordSqliteMapper.java
+++ src/main/java/com/haruhi/botServer/mapper/sqlite/ChatRecordSqliteMapper.java
@@ -3,7 +3,17 @@ package com.haruhi.botServer.mapper.sqlite;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.haruhi.botServer.entity.sqlite.ChatRecordSqlite;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ChatRecordSqliteMapper extends BaseMapper<ChatRecordSqlite> {
+
+    /**
+     * 分组统计群用户聊天记录
+     * @param groupId
+     * @return List<{ groupId,userId,total }>
+     */
+    List<ChatRecordSqlite> groupRecordCounting(@Param("groupId") Long groupId, @Param("selfId") Long selfId);
 }
