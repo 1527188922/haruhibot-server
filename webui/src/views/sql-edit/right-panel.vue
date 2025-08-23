b53a9a5939202d050d2d292fc074b1e30e0cb1d7
haruhi <1527188922@qq.com>
2025-08-23 22:19:09 +0800

    right-panel优化

--- webui/src/views/sql-edit/right-panel.vue
+++ webui/src/views/sql-edit/right-panel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="right-panel">
-    <el-container class="resizable-container">
+    <el-container>
       <el-main ref="main" :style="{ height: mainHeight + 'px' }">
           <sql-textarea v-model="content" @selection-change="handleSelection"></sql-textarea>
 
@@ -26,54 +26,56 @@ export default {
       dragBarHeight: 5,//拖动条高度
       mainHeight: 400,      // main初始高度
       footerHeight: 250,    // footer初始高度
-      minMainHeight: 300,   // main最小高度
-      minFooterHeight: 100, // footer最小高度
+      minMainHeight: 0,   // main最小高度
+      minFooterHeight: 30, // footer最小高度
       isDragging: false,
       content:'',
       selectedText:''
     }
   },
   mounted() {
-    this.mainHeight = this.$refs.main.$el.offsetHeight
-    document.addEventListener('mousemove', this.handleDrag)
-    document.addEventListener('mouseup', this.stopDrag)
+    this.mainHeight = this.$refs.main.$el.offsetHeight; // main初始高度
+    document.addEventListener('mousemove', this.handleDrag);
+    document.addEventListener('mouseup', this.stopDrag);
   },
   beforeDestroy() {
-    document.removeEventListener('mousemove', this.handleDrag)
-    document.removeEventListener('mouseup', this.stopDrag)
+    document.removeEventListener('mousemove', this.handleDrag);
+    document.removeEventListener('mouseup', this.stopDrag);
   },
   methods:{
     handleSelection(v){
-      this.selectedText = v
+      this.selectedText = v;
       console.log(this.content)
       console.log(this.selectedText)
     },
     startDrag(e) {
-      this.isDragging = true
-      this.startY = e.clientY
-      this.startMainHeight = this.mainHeight
-      document.body.style.cursor = 'ns-resize'
-      document.body.style.userSelect = 'none'
+      this.isDragging = true;
+      this.startY = e.clientY;
+      this.startMainHeight = this.mainHeight;
+      document.body.style.cursor = 'ns-resize';
+      document.body.style.userSelect = 'none';
     },
     handleDrag(e) {
-      if (!this.isDragging) return
-      const deltaY = e.clientY - this.startY
-      const newMainHeight = this.startMainHeight + deltaY
+      if (!this.isDragging) {
+        return
+      }
+      const deltaY = e.clientY - this.startY;
+      const newMainHeight = this.startMainHeight + deltaY;
 
       // 计算可用空间
-      const containerHeight = this.$el.clientHeight - this.dragBarHeight
-      const maxMainHeight = containerHeight - this.minFooterHeight
+      const containerHeight = this.$el.clientHeight - this.dragBarHeight;
+      const maxMainHeight = containerHeight - this.minFooterHeight;
 
       // 应用高度限制
       if (newMainHeight > this.minMainHeight && newMainHeight < maxMainHeight) {
-        this.mainHeight = newMainHeight
-        this.footerHeight = containerHeight - newMainHeight
+        this.mainHeight = newMainHeight;
+        this.footerHeight = containerHeight - newMainHeight;
       }
     },
     stopDrag() {
-      this.isDragging = false
-      document.body.style.cursor = ''
-      document.body.style.userSelect = ''
+      this.isDragging = false;
+      document.body.style.cursor = '';
+      document.body.style.userSelect = '';
     }
   }
 }
@@ -83,12 +85,6 @@ export default {
   display: flex;
   flex-direction: column;
   flex: 1;
-}
-
-.resizable-container {
-  flex: 1;
-  position: relative;
-  overflow: hidden;
 }
 
 .drag-bar {
