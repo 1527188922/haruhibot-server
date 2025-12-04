06a03778e1e50810c1465f9f05a23f1d01ee5966
刘昉 <liufang@yto.net.cn>
2025-12-04 15:23:00 +0800

    vue 路由转发到index.html

--- src/main/java/com/haruhi/botServer/config/WebServletConfig.java
+++ src/main/java/com/haruhi/botServer/config/WebServletConfig.java
@@ -2,19 +2,13 @@ package com.haruhi.botServer.config;
 
 import com.haruhi.botServer.interceptors.ApiHeaderInterceptor;
 import com.haruhi.botServer.interceptors.WebSocketHandshakeInterceptor;
-import com.haruhi.botServer.service.LoginService;
 import com.haruhi.botServer.utils.FileUtil;
 import com.haruhi.botServer.ws.BotServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -42,8 +36,7 @@ public class WebServletConfig implements WebSocketConfigurer, WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(apiHeaderInterceptor)
-                .addPathPatterns("/api/**")
-//                .excludePathPatterns("/error","/css/**", "/js/**", "/index.html", "/img/**", "/fonts/**", "/favicon.ico","/svg/**")
+                .addPathPatterns(BotConfig.CONTEXT_PATH+"/**")
         ;
     }
 
@@ -54,36 +47,5 @@ public class WebServletConfig implements WebSocketConfigurer, WebMvcConfigurer {
                 .addResourceLocations("classpath:/webui/")
                 .setCachePeriod(0);
         log.info("映射本地路径：{}",FileUtil.getAppDir());
-    }
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // 对于所有非API请求，返回index.html
-        registry.addViewController("/").setViewName("forward:/index.html");
-        registry.addViewController("/{path:[^\\.]*}").setViewName("forward:/index.html");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
-
-
-//    @Bean
-    public CorsFilter corsFilter(){
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.setAllowCredentials(true);
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-
-        config.addExposedHeader("Content-Type");
-        config.addExposedHeader( "X-Requested-With");
-        config.addExposedHeader("accept");
-        config.addExposedHeader("Origin");
-        config.addExposedHeader(LoginService.HEADER_KEY_AUTHORIZATION);
-        config.addExposedHeader(LoginService.HEADER_KEY_USER_NAME);
-        config.addExposedHeader( "Access-Control-Request-Method");
-        config.addExposedHeader("Access-Control-Request-Headers");
-
-        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-        configSource.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(configSource);
     }
 }
---
+++ src/main/java/com/haruhi/botServer/filter/VueHistoryFilter.java
@@ -0,0 +1,42 @@
+package com.haruhi.botServer.filter;
+
+import com.haruhi.botServer.config.BotConfig;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * Vue History 模式请求转发过滤器
+ */
+@Component
+@Order(2)
+public class VueHistoryFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String requestURI = request.getRequestURI();
+        String requestMethod = request.getMethod();
+
+        // true:需要转发
+        boolean need = HttpMethod.GET.name().equals(requestMethod)
+                        && !requestURI.startsWith(BotConfig.CONTEXT_PATH)
+                        && !requestURI.contains(".")
+                        && !"/index.html".equals(requestURI);
+
+        if (need) {
+            // 符合转发规则 → 服务器内部转发到 index.html
+            request.getRequestDispatcher("/index.html").forward(request, response);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
